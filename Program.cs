9fce2b4f1ac7ed31aa3f097b33d925bd9925dd43
srny0426 <srny0426@gmail.com>
2026-06-02 10:17:27 +0200

    Initial Release

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿// Velkomstbeskjed
+Console.WriteLine("Velkommen til butikken! Vi har disse produktene:   ");
+
+// matvare : rabattprosent : pris (kr)
+object[,] produkter = new object[3, 3] { { "ost", 1.0, 50 }, { "skinke", 0.9, 35 }, { "brød", 0.7, 20 } };
+
+bool ferdigÅHandle = false;
+
+// Liste for brukerens handlekurv
+List<string> handlekurv = new List<string> { };
+
+// Setter opp en loop for å handle som kun brytes når ferdigÅHandle endres
+while (!ferdigÅHandle)
+{
+    Console.Clear();
+
+    // Viser alle produktene butikken har til brukeren
+    for (int index = 0; index < produkter.GetLength(0); index++)
+    {
+        Console.Write($"{index + 1}:");
+        Console.WriteLine(produkter[index, 0]);
+    }
+    // Lar brukeren legge til produkter i handlekurven
+    Console.WriteLine("Legg et produkt i handlekurven. Velg ved å skrive tallet ved siden av produktet:     ");
+    int valg = int.Parse(Console.ReadLine()!) - 1;
+    handlekurv.Add((string)produkter[valg, 0]);
+
+    // Viser brukeren hva som er i handlekurven
+    Console.WriteLine("I handlekurven: ");
+    handlekurv.ForEach(Console.WriteLine);
+
+    // Sjekker om brukeren vil handle mer
+    Console.WriteLine("Er du ferdig å handle? 'ja' eller 'nei' :     ");
+    string ferdig = Console.ReadLine()!;
+    if (ferdig == "ja")
+        ferdigÅHandle = true;
+}
+// Legger sammen produktene i handlekurven og sjekker opp mot prisene og rabattene i produkter-arrayet
+double kalkulerPris()
+{
+    double total = 0;
+    foreach (string ting in handlekurv)
+    {
+        for (int i = 0; i < produkter.GetLength(0); i++)
+        {
+            if ((string)produkter[i, 0] == ting)
+            {
+                var prisMedRabatt = (int)produkter[i, 2] * (double)produkter[i, 1];
+                total += prisMedRabatt;
+            }
+        }
+
+    }
+    return total;
+}
+
+// Gir totalprisen til brukeren
+Console.WriteLine($"Totalen din blir kr {kalkulerPris()},-");
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Butikkoppgave:
+
+    1. Lager et array med produkter, rabattprosent og pris.
+    2. Setter opp en while loop som lar brukeren begynne handelen.
+    3. Lager en for loop som går gjennom alle produktene og skriver ut til brukeren, slik at de kan velge fra listen.
+    4. Lager en handlekurv-liste som brukeren kan legge produkter i ved å skrive et tall.
+    5. Viser handlekurven til brukeren og spør om de vil legge til flere produkter. Hvis svaret er nei, så slutter loopen.
+    6. Kalkulerer prisen på alt i handlekurven til brukeren, inkludert eventuell rabatt. Viser til brukeren.
